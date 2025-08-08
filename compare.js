52a9dfdd2a75927e136d0d89c1e091d1437dd1aa
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-09 01:06:40 +0600

    basic js comparison operator

---
+++ compare.js
@@ -0,0 +1,17 @@
+/* comparison
+
+*/
+
+console.log(5 > 6); // false
+
+console.log(5 < 6); // true
+
+console.log(5 >= 6); // false
+
+console.log(5 <= 6); // true
+
+console.log(5 == 6); // false
+
+console.log(5 != 6); // true
+console.log(5 == '5'); 
+console.log(5 === '5'); // false
---
+++ variable.js
@@ -0,0 +1,9 @@
+// variable
+var sunglasses = 400;
+
+var sub ='sunglasses';
+
+var passed = true;
+
+var lastExamResult = 100;
+console.log(typeof sunglasses);
