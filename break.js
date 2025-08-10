a9cd4d6ef6e83add5ca2944e34e7580711ab61d9
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-10 14:58:47 +0600

    practice break and continue

---
+++ break.js
@@ -0,0 +1,52 @@
+//break ------- i am done with this loop--------loop end
+
+/* let n = 5;
+
+for(let i = 0; i<5; i++){
+
+    if(i==2){
+        break;
+    }
+
+
+    console.log(i);
+}
+ */
+
+
+/* let n =5;
+while(n>0){
+    if(n==3){
+        break;
+    }
+    console.log(n);
+    n--;
+} */
+
+
+//continue---------skip this iteration
+
+//even number using the continue 
+
+/* for(let i = 1; i<=10;i++){
+    i++;
+    if(i%2==1){
+        continue;
+    }
+    console.log(i);
+}
+ */
+
+//continue  means skip this iteration
+//odd number using the continue
+
+let n =0;
+while( n<=10){
+    n++;
+    if(n%2==0){
+        continue;
+    }
+
+    console.log(n);
+    
+}
