c73fba71720890f6a24203f7a7e4024370b69813
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-10 13:12:11 +0600

    practice on for loop

--- for.js
+++ for.js
@@ -1,4 +1,4 @@
-const value = [1, 2, 3, 4, 5];
+/* const value = [1, 2, 3, 4, 5];
 for(const num of value){
     console.log(num);
 }
@@ -9,4 +9,16 @@ const name = ["Belayet","Hossain","Hasan","Rio"]
 for(const person of name){
     console.log(person);
     console.log("Hello, ")
+}
+
+
+ */
+
+for(let i =1; i<=20; i++){
+    if (i%2 ==0){
+        console.log("Even number is ", i);
+    }
+    else{
+        console.log("Odd number is ", i);
+    }
 }
