146186bfd1195139cf02285dc739bc639a67888a
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-10 12:46:45 +0600

    practice for loop

---
+++ for.js
@@ -0,0 +1,12 @@
+const value = [1, 2, 3, 4, 5];
+for(const num of value){
+    console.log(num);
+}
+
+
+
+const name = ["Belayet","Hossain","Hasan","Rio"]
+for(const person of name){
+    console.log(person);
+    console.log("Hello, ")
+}
