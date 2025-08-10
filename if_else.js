4eb40e00370b91d0b3f55cec9019588487334eb2
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-10 10:13:50 +0600

    practice ternary operator

---
+++ if_else.js
@@ -0,0 +1,13 @@
+const salary = 50000000;
+
+if(salary > 60000){
+    console.log('Good');
+}
+
+else if(salary>60000){
+    console.log('Average');
+}
+
+else{
+    console.log('Poor');
+}
---
+++ ternary.js
@@ -0,0 +1,15 @@
+/* ternary operator */
+
+const balance = 599;
+// simple ternary operator 
+
+balance > 500 ? console.log("You can purchase"):console.log("You cannot purchase");
+
+let flower = true;
+let smile = false;
+
+// ternary operator 
+
+let feel = flower == true ? smile==true ? 'happy' : 'smile please' : 'sad';
+
+console.log(feel);
