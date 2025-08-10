b078684de258f385ed1fbacfb8feddf150eaa7b6
belayet-hossain <mbelayethossain202@gmail.com>
2025-08-10 13:06:20 +0600

    practice on while loop

---
+++ while.js
@@ -0,0 +1,30 @@
+// let num1 = 10;
+
+// while(num1>0){
+//     console.log(num1);
+//     num1--
+// }
+
+// sum first 10 num
+
+/* let num =1;
+let sum =0;
+while(num<=10){
+    console.log(num);
+    sum = sum +num;
+    console.log("current sum: " , sum);
+    num++;
+
+} */
+
+// even number
+
+let num2 = 1;
+while(num2<=10){
+    console.log(num2);
+    if(num2%2==0){
+        console.log("Even num is ", num2);
+    }
+
+    num2++;
+}
